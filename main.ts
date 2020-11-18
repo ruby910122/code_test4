c9f302114b9e0663a4179ab0703b00d114d97f9c
ruby910122 <71751285+ruby910122@users.noreply.github.com>
2020-11-18 15:21:59 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    for (let y = 0; y <= 2; y++) {
+        for (let x = 0; x <= 2; x++) {
+            if (2 - x == 2 || 2 + (y - x) == 2) {
+                led.plot(2 + x, 2 - (y - x))
+                led.plot(2 - x, 2 + (y - x))
+            }
+        }
+        basic.pause(200)
+    }
+})
